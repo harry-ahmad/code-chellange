38165d4a0ceff0b8d958f0f89cf9d1ba98b3d97d
Harry Ahmad <haroon.ahmad.cs@gmail.com>
2023-08-31 00:18:46 +0500

    worked on indentation and changed time format from am/pm to 24 hours

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -3,25 +3,29 @@
 namespace App\Database;
 
 use PDO;
+use Exception;
 
-class Database {
-    /** @var Database $connection  */
+class Database
+{
+    /** @var Database $connection */
     private static $instance;
-    /** @var PDO $connection  */
+    /** @var PDO $connection */
     private $connection;
 
-    private function __construct(){
-        if(empty(self::$instance)){
-            try{
+    private function __construct()
+    {
+        if (empty(self::$instance)) {
+            try {
                 $this->connection = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch(\Exception $e) {
-                echo "Connection failed : " . $e->getMessage();
+            } catch (\Exception $e) {
+                throw new Exception("Connection failed : " . $e->getMessage());
             }
         }
     }
 
-    public static function getInstance(): Database{
+    public static function getInstance(): Database
+    {
         if (self::$instance == null) {
             self::$instance = new Database();
         }
